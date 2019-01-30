7a0420714b473541d07d1d2f9f582745147c2ef4
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-01-30 15:14:58 -0600

    added an action (page_clicked) that updates the database to show the page has been clicked
    - updating happens to the LClick row of the KeyMouseEventStat table
    where times_triggered is increased by 1 and the change is saved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
     def home
         @LeftClickEventStatistic = KeyboardMouseEventStatistic.find_by event_type: 'LClick'
     end
+
+    def page_clicked
+        @LeftClickEventStatistic = KeyboardMouseEventStatistic.find_by event_type: 'LClick'
+        @LeftClickEventStatistic.update(times_triggered: @LeftClickEventStatistic.times_triggered + 1)
+        @LeftClickEventStatistic.save
+        render 'home'
+    end
 end
