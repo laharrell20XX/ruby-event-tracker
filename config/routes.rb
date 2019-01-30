8fc7080c22dbf90b81e6ab956d406ba8c7722166
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-01-30 15:20:18 -0600

    added a route that goes to the page_clicked action on the pages controller whenever a GET request is made to "page_clicked"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'home' => 'pages#home'
+  get 'page-clicked' => 'pages#page_clicked'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
