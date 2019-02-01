76e66631d749b28f32e649fe299ecf76b9b0949d
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-01 14:58:59 -0600

    added a route to the spacebar_pressed action in the pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home' => 'pages#home'
   get 'page-clicked' => 'pages#page_clicked'
+  get 'space-pressed' => 'pages#spacebar_pressed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
