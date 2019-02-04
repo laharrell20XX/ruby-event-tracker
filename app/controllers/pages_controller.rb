34d20793a718ae85f9c05804daf706504692b61b
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-03 18:01:43 -0600

    modified the event_triggered actions to go to the StaticEventStat migration; changed the homepage to store the totals of events; event_triggered actions now add the event to StaticEvent

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,23 @@
 class PagesController < ApplicationController
 
     def home
-        @KeyboardMouseEventStatistics = KeyboardMouseEventStatistic.all
+        @KeyboardMouseEventStatistics = StaticEventStatistic.all
+        @LeftClickCount = StaticEvent.where(event_type: 'LClick').count
+        @SpaceBarPressCount = StaticEvent.where(event_type: 'SpaceBarPress').count
     end
 
     def page_clicked
-        @LeftClickEventStatistic = KeyboardMouseEventStatistic.find_by event_type: 'LClick'
+        @LeftClickEventStatistic = StaticEventStatistic.find_by event_type: 'LClick'
         @LeftClickEventStatistic.update(times_triggered: @LeftClickEventStatistic.times_triggered + 1)
         @LeftClickEventStatistic.save
         redirect_to action: :home
+        StaticEvent.create(event_type: "LClick")
     end
     def spacebar_pressed
-        @SpacebarEventStatistic = KeyboardMouseEventStatistic.find_by event_type: 'SpaceBarPress'
+        @SpacebarEventStatistic = StaticEventStatistic.find_by event_type: 'SpaceBarPress'
         @SpacebarEventStatistic.update(times_triggered: @SpacebarEventStatistic.times_triggered + 1)
         @SpacebarEventStatistic.save
         redirect_to action: :home
+        StaticEvent.create(event_type: "SpaceBarPress")
     end
 end
