041ac68262e5a01a96bd56cae608a83394643284
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-06 21:21:19 -0600

    added class method update_ev;
    - update_ev takes in an event_type and update the times_triggered
    column by one for that event

--- app/models/static_event_statistic.rb
+++ app/models/static_event_statistic.rb
@@ -1,2 +1,6 @@
 class StaticEventStatistic < ApplicationRecord
+    def self.update_ev(event_type)
+        event = self.find_by event_type: event_type
+        event.update(times_triggered: event.times_triggered + 1)
+    end
 end
