e6e37efdf36e2330e3e8946a38b42b58d24a7e6c
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-06 21:23:49 -0600

    added class method daily_events
    - daily_events takes in a day and event_type and queries events that
    happened that day; returns a StaticEvent record

--- app/models/static_event.rb
+++ app/models/static_event.rb
@@ -1,2 +1,5 @@
 class StaticEvent < ApplicationRecord
+    def self.daily_events(day, event_type)
+        where("updated_at >= ? AND updated_at <= ? AND event_type = ?", day, day + 1.day, event_type)
+    end
 end
