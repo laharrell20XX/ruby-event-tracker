5c9da4306aa4c2baa7e2ef6131f0183e0400c2fa
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-06 21:04:21 -0600

    changed 'page-clicked' route to 'button-clicked' instead; added a route to daily-statistics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home' => 'pages#home'
-  get 'page-clicked' => 'pages#page_clicked'
+  get 'button-clicked' => 'pages#button_clicked'
   get 'space-pressed' => 'pages#spacebar_pressed'
+  get 'daily-statistics' => 'pages#daily_stats'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
