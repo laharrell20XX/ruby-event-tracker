f5b232f56dd74d8b875d70178f22987ffe49ddcc
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-13 23:49:54 -0600

    added update_color_ev method for events that need color updated

--- app/models/static_event_statistic.rb
+++ app/models/static_event_statistic.rb
@@ -3,4 +3,8 @@ class StaticEventStatistic < ApplicationRecord
         event = self.find_by event_type: event_type
         event.update(times_triggered: event.times_triggered + 1)
     end
+    def self.update_color_ev(event_type, color)
+        event = self.find_by event_type: event_type
+        event.update(times_triggered: event.times_triggered + 1, color: color)
+    end
 end
