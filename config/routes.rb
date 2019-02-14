4b7ed736a88120d0eb2e8540317635bc86968e0c
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-13 23:25:11 -0600

    added a post route to /box-colored-changed and the box_colored_changed action on the pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'button-clicked' => 'pages#button_clicked'
   get 'space-pressed' => 'pages#spacebar_pressed'
   get 'daily-statistics' => 'pages#daily_stats'
+  post 'box-color-changed' => 'pages#box_color_changed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
